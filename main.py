595e8597501a7f051c127d9257d64635f5ff733f
Endless <heisgenburgthecook@gmail.com>
2025-01-31 13:56:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,19 @@ handler = ArgumentHandler()
 print("Welcome to the Github Repo Creation Utility Created by EndlessSB")
 
 name = input("Enter the Repo Name --> ")
+description = input("Leave blank if you dont want a description --> ")
+if not description:
+    yorn = input("You haven't provided a description are you sure? [y/n] --> ")
+    if yorn == "n":
+        exit()
 visibility = input("public Or private --> ").lower()
 readme = input("Add a readme? [y/n] --> ").lower()
 clone = input("Clone it? [y/n] --> ").lower()
+issues = input("Enable issues? --> ")
+template = input("Do you want to base this off a template and if so provide a link if not leave blank --> ")
 extra_args = input("Any args you wish to include which are not already include [if none leave blank] --> ")
 confirm = input("Are you sure you want to continue this action? [y/n] --> ").lower()
+
 
 if extra_args:
     handler.add_arg(extra_args)
@@ -24,8 +32,8 @@ if confirm == "n":
 if not name:
     print("You need to specify a name")
     exit()
-
-
+if description:
+    handler.add_arg(f"--description \"{description}\"")
 if visibility == "public":
     handler.add_arg("--public")
 elif visibility == "private":
@@ -37,6 +45,12 @@ if readme == "y":
     handler.add_arg("--add-readme")
 if clone == "y":
     handler.add_arg("-c")
+if issues == "y":
+    handler.add_arg("--enable-issues")
+elif issues == "n":
+    handler.add_arg("--disable-issues")
+if template:
+    handler.add_arg(f"--template \"{template}\"")
 
 args_str = ' '.join(handler.return_args())
 
